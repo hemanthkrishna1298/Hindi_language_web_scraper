3b4470b7f0b86e6098e352a9b4d8212780db344a
Hemantha Krishna Bharadwaj <hemanthkrishna1298@gmail.com>
2021-10-20 12:59:18 +0530

    Update collect_links_from_excel_and_scrape_text.py

--- manual_collection_scraping/collect_links_from_excel_and_scrape_text.py
+++ manual_collection_scraping/collect_links_from_excel_and_scrape_text.py
@@ -24,22 +24,22 @@ class DataScraperFromLinks():
         This function collects links from an excel spreadsheet and appends them to a Python list. Then it groups links from same websites together, so that you can go through each website, write the scraper function for it, and then collect text from all links of the same website together.
         """
         # Collecting all links from the excel sheet and sorting them to group same website's links together
-        # my_workbook = openpyxl.load_workbook(excel_filename)
-        # for sheet in my_workbook.sheetnames:
-        #     worksheet = my_workbook[sheet]
-        #     for row in worksheet.iter_rows(min_col=5):
-        #         for cell in row:
-        #             try:
-        #                 link = cell.hyperlink.target
-        #             except:
-        #                 link = None
-        #             if(link is not None):
-        #                 self.list_of_links.append(link)
-        # print(f"The number of links collected is {len(self.list_of_links)}.")
+        my_workbook = openpyxl.load_workbook(excel_filename)
+        for sheet in my_workbook.sheetnames:
+            worksheet = my_workbook[sheet]
+            for row in worksheet.iter_rows(min_col=5):
+                for cell in row:
+                    try:
+                        link = cell.hyperlink.target
+                    except:
+                        link = None
+                    if(link is not None):
+                        self.list_of_links.append(link)
+        print(f"The number of links collected is {len(self.list_of_links)}.")
 
         ## temporary  code
-        with open('finance_list_of_links', 'rb') as fp:
-            self.list_of_links = pickle.load(fp)
+        # with open('finance_list_of_links', 'rb') as fp:
+        #     self.list_of_links = pickle.load(fp)
 
         self.list_of_links.sort(key= self.list_of_links_sorting_utility)
         
@@ -70,46 +70,46 @@ class DataScraperFromLinks():
 
 
         
-        # # Iterate through the links acc to dictionary and for each website define the text scraping and scrape text
-        # j = 0    
-        # k = 2017 #change k to value in "position_in_program" in case resuming incomplete scraping
-        # for website, count in self.url_dict.items():
-        #     i= count
-        #     #update scraping function here....
-        #     try:
-        #         if(j>=k):
-        #             webbrowser.open(self.list_of_links[j])    #open the first url of each website to edit the scraping function accordingly
-        #     except:
-        #         print(f"Website didn't open! Open this link: {self.list_of_links[j]}")   
-        #     if(j>=k):
-        #         print(f"Website currently being scraped: {website}, Count: {count}")
-        #         index = list(self.url_dict.keys()).index(website)
-        #         print(f"The index of website {website} is {index}")
-        #         checkinput = input("Press enter once you're done changing the scraping function. Press 0 to exit.")
-        #         if(checkinput=="0"):
-        #             break
-        #         reload(scrape_text)
+        # Iterate through the links acc to dictionary and for each website define the text scraping and scrape text
+        j = 0    
+        k = 2017 #change k to value in "position_in_program" in case resuming incomplete scraping
+        for website, count in self.url_dict.items():
+            i= count
+            #update scraping function here....
+            try:
+                if(j>=k):
+                    webbrowser.open(self.list_of_links[j])    #open the first url of each website to edit the scraping function accordingly
+            except:
+                print(f"Website didn't open! Open this link: {self.list_of_links[j]}")   
+            if(j>=k):
+                print(f"Website currently being scraped: {website}, Count: {count}")
+                index = list(self.url_dict.keys()).index(website)
+                print(f"The index of website {website} is {index}")
+                checkinput = input("Press enter once you're done changing the scraping function. Press 0 to exit.")
+                if(checkinput=="0"):
+                    break
+                reload(scrape_text)
 
-        #     #use scraping function to scrape for each website
+            #use scraping function to scrape for each website
             
-        #     while(i>0):
+            while(i>0):
                 
-        #         if(k>j):
-        #             i-=1
-        #             j+=1
-        #             continue
-        #         flag = scrape_text.scrape_text(j, self.list_of_links[j])
-        #         if(flag):
-        #             link_exceptions.append(self.list_of_links[j])
-        #         # print(flag)
-        #         i-=1
-        #         j+=1
-        #         with open('position_in_program.txt', 'w') as f1:
-        #             f1.write(str(j))
-        # with open(os.path.join('exceptionlinks.txt'), 'a') as f:
-        #     for exceptionlink in link_exceptions:
-        #         f.write(exceptionlink)
-        #         f.write('\n')
+                if(k>j):
+                    i-=1
+                    j+=1
+                    continue
+                flag = scrape_text.scrape_text(j, self.list_of_links[j])
+                if(flag):
+                    link_exceptions.append(self.list_of_links[j])
+                # print(flag)
+                i-=1
+                j+=1
+                with open('position_in_program.txt', 'w') as f1:
+                    f1.write(str(j))
+        with open(os.path.join('exceptionlinks.txt'), 'a') as f:
+            for exceptionlink in link_exceptions:
+                f.write(exceptionlink)
+                f.write('\n')
 
 datascraper = DataScraperFromLinks()
 datascraper.copy_links_from_excel_and_scrape_text(excel_filename= 'HINDI-HEMANTHA.xlsx')
